ebb046087051e3a6250ddc7c0bc85a33413953ae
chris_beltran <cristianb72@hotmail.com>
2024-08-15 14:47:50 -0600

    adding models commit

---
+++ back/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const UserSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    surname:String,
+    nick:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        default:"role_user"
+    },
+    image:{
+        type:String,
+        default:"default.png"
+    },
+    created_at:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+
+module.exports=mongoose.model("User",UserSchema,"users");
